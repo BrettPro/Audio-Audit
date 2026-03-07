008f53c0e0ab1b1b448b21755205bc92e7047da9
BrettPro <brettsestak@utexas.edu>
2026-03-06 18:25:04 -0600

    Initial settings work in progress

--- AudioAudit/SettingsViewController.swift
+++ AudioAudit/SettingsViewController.swift
@@ -9,21 +9,47 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var fontLabel: UILabel!
+    
+    @IBOutlet weak var modeImage: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fontLabel.text = "Current Font Size: 3"
+        modeImage.tintColor = .label
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func modeButton(_ sender: UIButton) {
+        if overrideUserInterfaceStyle == .dark {
+            overrideUserInterfaceStyle = .light
+            sender.setImage(UIImage(systemName: "sun.max.fill")?.withRenderingMode(.alwaysTemplate), for: .normal)
+            modeImage.tintColor = .white
+        } else {
+            overrideUserInterfaceStyle = .dark
+            sender.setImage(UIImage(systemName: "moon.fill")?.withRenderingMode(.alwaysTemplate), for: .normal)
+            modeImage.tintColor = .black
+        }
     }
-    */
-
+    @IBAction func mapButton(_ sender: Any) {
+    }
+    @IBAction func fontSelector(_ sender: UISlider) {
+        let roundedValue = round(sender.value)
+        sender.value = roundedValue
+        fontLabel.text = "Current Font Size: \(Int(roundedValue))"
+        
+        
+    }
+    @IBAction func logoutButton(_ sender: Any) {
+        let alert = UIAlertController(
+                title: "Log Out",
+                message: "Are you sure you want to log out?",
+                preferredStyle: .alert
+            )
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let logoutAction = UIAlertAction(title: "Log Out", style: .destructive)
+        alert.addAction(cancelAction)
+        alert.addAction(logoutAction)
+        present(alert, animated: true)
+    }
 }
